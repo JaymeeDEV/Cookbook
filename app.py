99b1bdcc698a9db0de40ca7bf10ca5188793ab08
JaymeeDEV <jamiekellybrown@gmail.com>
2019-11-20 16:49:23 +0000

    Remove registration and improve contact page

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 from config import Config
-from flask import Flask, render_template, redirect, request, url_for, session
+from flask import Flask, render_template, redirect, request, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -31,33 +30,6 @@ def popular():
 @app.route('/contact', methods=['POST', 'GET'])
 def contact():
     return render_template("contact.html")
-
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        user_in_db = db.users.find_one({
-            "username": request.form['username']
-        })
-        print(user_in_db)
-        if user_in_db and check_password_hash(user_in_db['password'], request.form['password']):
-            session["username"] = user_in_db["username"]
-            return redirect(url_for('index'))
-        else:
-            db.users.insert_one({
-                "username": request.form['username'],
-                "email": request.form['email'],
-                "password": generate_password_hash(request.form['password'])
-            })
-            return redirect(url_for('index'))
-    else:
-        return render_template("register.html")
-
-
-@app.route('/logout', methods=['POST', 'GET'])
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
 
     """ Recipe Pages"""
 
